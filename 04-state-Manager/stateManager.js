d62c3a0bcab7dd4686c09b8c6afe78d278d205c6
Magesh <tkmagesh77@gmail.com>
2018-02-01 14:39:06 +0530

    state manager implmentation

---
+++ 04-state-Manager/stateManager.js
@@ -0,0 +1,40 @@
+
+var SM = (function(){
+	let _currentState = undefined,
+		_listenerFns = [],
+		_reducer = null,
+		_init_action = '@@INIT_ACTION';
+
+	function getState(){
+		return _currentState;
+	}
+
+	function subscribe(listenerFn){
+		if (typeof listenerFn === 'function')
+			_listenerFns.push(listenerFn);
+	}
+
+	function triggerChange(){
+		_listenerFns.forEach(listenerFn => listenerFn());
+	}
+
+	function dispatch(action){
+		let result = _reducer(_currentState, action);
+		if (result === _currentState) return;
+		_currentState = result;
+		triggerChange();
+	}
+
+	function createStore(reducer){
+		_reducer = reducer;
+		_currentState = reducer(_currentState, _init_action);
+		return {
+			getState : getState,
+			subscribe : subscribe,
+			dispatch : dispatch
+		}
+	}
+	return {
+		createStore : createStore
+	}
+})();
